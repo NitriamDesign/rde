7a7f86baf7ab971a9c399c4a30dfd4c1c09a36ed
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-03 14:45:07 -0400

    CTA Ribbon
    Worked on intial setup for cta ribbon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import ProductCategories from "$lib/homepage/products/ProductCategories.svelte";
 import TopHero from "$lib/homepage/TopHero.svelte";
+import CallToActionRibbon from "$lib/pre-constructs/CallToActionRibbon.svelte";
 </script>
 
 
 <TopHero/>
 <ProductCategories/>
+<CallToActionRibbon bgImageUrl="/standin/dum.png"/>
