82239b05365fbe9b309f5d3e10b69749b416979e
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-03 14:49:53 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import ProductCategories from "$lib/homepage/products/ProductCategories.svelte";
     import TopHero from "$lib/homepage/TopHero.svelte";
     import ProductCardHorizontal from "$lib/homepage/products/ProductCardHorizontal.svelte";
+    import CallToActionRibbon from "$lib/pre-constructs/CallToActionRibbon.svelte";
 
 </script>
 
@@ -9,3 +10,4 @@
 <TopHero/>
 <ProductCategories/>
 <ProductCardHorizontal/>
+<CallToActionRibbon bgImageUrl = '/standin/dum.png'/>
