e16eb787ad4f9045ed31470628dec15f2056d9c4
DeLayne Russell <teesitin.russell@gmail.com>
2024-04-03 17:00:55 -0400

    general footer structure

---
+++ src/lib/navigation/Footer.svelte
@@ -0,0 +1,81 @@
+<script>
+    import { MapPinAltSolid, PhoneSolid, MessagesSolid, InfoCircleSolid } from 'flowbite-svelte-icons';
+
+</script>
+
+
+<div class="bg-m-light h-44 flex items-center">
+    <div class="flex justify-between max-w-7xl m-auto text-m-dark gap-20">
+        <div class="">
+            <div class="text-xl font-semibold uppercase mb-4">    
+                RDE CONNECTS
+            </div>
+            
+            <div class="flex flex-col gap-2 text-md font-semibold uppercase text-m-primary2">
+                <div>    
+                    Products
+                </div>
+
+                <div>    
+                    Guides
+                </div>
+
+                <div>    
+                    About Us
+                </div>
+            </div>
+        </div>
+
+        <div class="">    
+            <div class="text-xl font-semibold uppercase mb-4">    
+                Contact
+            </div>
+            
+            <div class="flex flex-col gap-2 text-md font-semibold text-m-primary2">
+                <div class="flex items-center gap-2">    
+                    <MapPinAltSolid/>586 Fulling Mill Road, Middletown, PA 17057
+                </div>
+
+                <div class="flex items-center gap-2">   
+                    <PhoneSolid/>(800) 888-8888
+                </div>
+
+                <div class="flex items-center gap-2">   
+                    <MessagesSolid/>us-info@phoenixcontact.com
+                </div>
+            </div>
+        </div>
+
+        <div class="">    
+            <div class="text-xl font-semibold uppercase mb-4">    
+                ISO 9001-2015 Certified
+            </div>
+            
+            <div class="flex flex-col gap-2 text-md font-semibold  text-m-primary2">
+                <div class="flex items-center gap-2">  
+                    <InfoCircleSolid/>Information
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="bg-m-dark h-20">
+    <div class="flex justify-center max-w-7xl m-auto  h-full items-center text-gray-300 gap-20">
+        <div>    
+            © Copyright 2024 - RDE Connectors & Cables, Inc.
+        </div>
+
+        <a href="/">    
+            Site notice
+        </a>
+
+        <a href="/">    
+            Legal information
+        </a>
+    </div>
+</div>
