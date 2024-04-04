b0bf446a785fbe66d6f3834b677ac9009eceba63
DeLayne Russell <teesitin.russell@gmail.com>
2024-04-04 14:39:49 -0400

    global anchor-assist

--- src/lib/homepage/TopHero.svelte
+++ src/lib/homepage/TopHero.svelte
@@ -2,18 +2,7 @@
     import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
     import { Button } from 'flowbite-svelte';
     import { ArrowDownOutline } from 'flowbite-svelte-icons';
-
-	function handleAnchorClick (event:any, anchorId: string) {
-		event.preventDefault();
-		const anchor = document.getElementById(anchorId);
-
-        if(anchor) {
-            window.scrollTo({
-                top: anchor.offsetTop - 60,
-                behavior: 'smooth'
-            })
-        }
-	}
+    import {handleAnchorClick} from '$lib/anchors'
 </script>
 
 <div class="bg-[#C4C4C4] h-[600px] flex justify-center">
@@ -26,7 +15,9 @@
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.
         </div>
 
-        <Button color="none" class="bg-m-primary max-w-sm m-auto uppercase text-m-light hover:bg-primary-600 rounded text-md font-thin" on:click={event => handleAnchorClick(event, "main-products")}>
+        <Button color="none" class="bg-m-primary max-w-sm m-auto uppercase text-m-light hover:bg-primary-600 rounded text-md font-thin"
+            on:click={event => handleAnchorClick(event, "main-products", 60)}
+        >
             View Products <ArrowDownOutline size="lg" class="ml-2"/>
         </Button>
     </div>
