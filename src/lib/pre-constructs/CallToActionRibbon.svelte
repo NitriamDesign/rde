31cc755003051677345a55281ba9e72e56f02ec8
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-09 20:40:54 -0400

    Update CallToActionRibbon.svelte

--- src/lib/pre-constructs/CallToActionRibbon.svelte
+++ src/lib/pre-constructs/CallToActionRibbon.svelte
@@ -38,14 +38,14 @@
 
 
 <div class="bg-ribbon w-screen">
-  <div class="flex md:w-[30%] m-auto relative items-center" style="height: 250px;">
-      <div class="h-[325px] bg-white w-full absolute right-[47%] z-10 -top-6 p-10 pr-[4rem] shadow-xl">
+  <div class="md:flex lg:w-[30%] m-auto relative items-center" style="height: 250px;">
+      <div class="lg:h-[325px] lg:absolute lg:right-[47%] bg-white w-[564px] z-10 -top-6 p-10 pr-[4rem] shadow-xl">
         <span class="self-center text-xl font-semibold text-[#C4C4C4] uppercase italic font-sans">Member of the Phoenix Contact Group</span>
         <h5 class="mb-2 text-4xl font-bold text-gray-900 dark:text-white">We Have Worked With This Company!</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <Button href="#" size="xl">ABOUT US</Button>
       </div>
 
-      <div class="h-[325px]  w-full relative left-[47%] z-10 top-[70px] shadow-xl" style="background-image: url({bgImageUrl});"></div>
+      <div class="lg:h-[325px] lg:left-[47%] w-full relative z-10 top-[70px] shadow-xl" style="background-image: url({bgImageUrl});"></div>
   </div>  
 </div>
