2b25025b78636e7aca01198484e84d87ca7f6bb1
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-15 17:48:06 -0400

    svelte pdf viewer

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -1,6 +1,8 @@
-<script>
-
+<script lang="ts">
+    import { PdfViewer } from "svelte-pdf-simple";
 </script>
 
+<PdfViewer 
+  props={{path: "/crimp-tools.pdf"}} 
+  style="border: 1px solid black; display: block; margin-top: 10px;"/>
 
-guides
