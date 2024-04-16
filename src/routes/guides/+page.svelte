bdbd5fdd418c55835fd46734599b4306078725e8
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-16 18:03:08 -0400

    pdf problems

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  import PDFViewer from "$lib/pre-constructs/PDFViewer.svelte"
     import { onMount } from "svelte";
     let PdfViewer;
     let url = "/pdf/crimp-tools.pdf";
@@ -8,9 +7,7 @@
     const module = await import("svelte-pdf");
     PdfViewer = module.default;
   });
+
 </script>
 
 <svelte:component this={PdfViewer} url={url} showButtons={["navigation", "zoom", "print", "download", "pageInfo"]}/>
-
-
-<PDFViewer/>
