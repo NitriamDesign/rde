c75922e306c594e4fa092298824313ef94af4242
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-23 15:20:28 -0400

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,6 +5,18 @@ export interface Product {
     mainImage: string;
     pdf: string;
     url: string;
+    shell_mat?: Entry[];
+    insulator?: Entry[];
+    contacts?: Entry[];
+    sealing?: Entry[];
+    temperature?:Entry[];
+    mating?: Entry[];
+    protection?:Entry[];
+}
+
+export interface Entry {
+    title: string;
+    description: string;
 }
 
 export const products: Product[] = [
@@ -14,7 +26,44 @@ export const products: Product[] = [
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
         mainImage: "/standin/dum.png",
         pdf: "/pdf/crimp-tools.pdf",
-        url: "/products/tu-connector"
+        url: "/products/tu-connector",
+        shell_mat: [   
+                    {title: "Machined components", description: "Copper-Zinc alloy (CuZn)"}, 
+                    {title: "Die-Cast components", description: "Zinc (GD-Zn)"}, 
+                    {title: "Shell plating", description: "Nickel (Ni) plated"}, 
+                    {title: "Cable entry", description: "max. 19 mm"}
+                ],
+
+        insulator: [   
+            {title: "Connector inserts", description: "Copper-Zinc alloy (CuZn)"}
+        ],
+        
+        contacts: [   
+            {title: "Contact material", description: "Copper-Zinc alloy (CuZn)"}, 
+            {title: "Contact plating", description: "Nickel (Ni) with gold (Au) passivated"}, 
+            {title: "Contact termination", description: "crimp type"}, 
+        ],
+
+        sealing: [   
+            {title: "Sealing rings", description: "Fluor rubber FPM"}, 
+            {title: "Gaskets", description: "Fluor rubber FPM"}, 
+        ],
+
+        temperature: [   
+            {title: "Temperature (long term) Celsius", description: "- 20 °C up to + 125°C"}, 
+            {title: "Temperature (long term) Fahrenheit", description: "- 4 °F up to + 257 °F"}, 
+        ],
+
+        mating: [   
+            {title: "Mating cycles", description: "Standard min. 50"}, 
+            {title: "Female coupling nuts", description: "M23 screw locking"},
+            {title: "Male receptacles", description: "M23 screw locking"}
+        ],
+
+        protection: [   
+            {title: "Protection class", description: "IP 67 (mated) depends on which cable gland is used"}
+        ],
+
     },
     {
         name: "CA Series Connectors",
