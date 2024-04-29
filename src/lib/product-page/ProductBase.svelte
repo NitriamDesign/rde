96a6da59140d46133e8da8cd84144a818847815c
117GuiltySpark <54770530+117GuiltySpark@users.noreply.github.com>
2024-04-29 14:36:38 -0400

    work on product page layout and pdf viewing

--- src/lib/product-page/ProductBase.svelte
+++ src/lib/product-page/ProductBase.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
     import { type Product } from "$lib/data"
     
-    import {Table,TableBody,TableBodyCell,TableBodyRow,TableHead,TableHeadCell,ImagePlaceholder, Modal} from 'flowbite-svelte';
+    import {Table,TableBody,TableBodyCell,TableBodyRow,TableHead,TableHeadCell,ImagePlaceholder, Modal, Card} from 'flowbite-svelte';
     import { slide } from 'svelte/transition';
     export let product: Product
     import { AccordionItem, Accordion, Button } from 'flowbite-svelte';
-    
+    import { Heading, P, Span, List, Li } from 'flowbite-svelte';
+  
+ 
     const items = Array(3);
+    let categoryNames = [['Shell Material', 'shell_mat'], ['Insulator','insulator'],  ['Contacts','contacts' ], ['Sealing','sealing'], ['Temperature', 'temperature']]
 
 const open_all = () => items.forEach((_, i) => (items[i] = true));
 const close_all = () => items.forEach((_, i) => (items[i] = false));
-
-
 </script>
 
-<div class="max-w-7xl m-auto mt-20 justify-between gap-10 flex flex-col md:flex-row p-4">
+<div class="max-w-7xl m-auto mt-20 mb-20 justify-between gap-10 flex flex-col md:flex-row ">
+
     <div>
         <div class="text-left text-md">
             <div class="text-3xl font-semibold text-m-primary mb-2">
@@ -36,86 +38,37 @@ const close_all = () => items.forEach((_, i) => (items[i] = false));
     </div>
 
     <img src={product.mainImage} alt="example" class="rounded w-[500px]">
+
+    
 </div>
 
-<div class="max-w-7xl m-auto">
+<Heading tag="h1" class="mt-10 md:mt-0 max-w-7xl m-auto" customSize="text-3xl font-extrabold  md:text-4xl lg:text-4xl">
+  Technical Data
+</Heading>
 
-<Table color="green" class="mt-20" hoverable={true}>
-  <TableHead>
-    <TableHeadCell>Shell Material</TableHeadCell>
-    <TableHeadCell>Insulator</TableHeadCell>
-    <TableHeadCell>Contacts</TableHeadCell>
-    <TableHeadCell>Sealing</TableHeadCell>
-    <TableHeadCell>Temperature</TableHeadCell>
-    <TableHeadCell>Mating</TableHeadCell>
-    <TableHeadCell>Protection</TableHeadCell>
-  </TableHead>
-  <TableBody tableBodyClass="divide-y">
-    <TableBodyRow>
+<div class="max-w-7xl mx-auto flex flex-col md:flex-row items-start mt-10">
+   <!-- Material information container -->
+   <div class="flex-2">
 
-      <TableBodyCell>
-        {#if product.shell_mat}
-        {#each product.shell_mat ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-      </TableBodyCell>
-      
+    {#each categoryNames as [headerName, dataName]}
+    <Card class="rounded-none mb-5">
+      <Heading tag="h2" class="mb-2 text-2xl font-extrabold">
+        {headerName}
+      </Heading>
+      {#if product[dataName]}
+    {#each product[dataName] ?? [] as entry}
+    <strong>{entry.title}:</strong> {entry.description}<br>
+    {/each}
+    {/if}
+    </Card>
+    {/each}
+    
+  </div>
 
-      <TableBodyCell>
-        {#if product.insulator}
-        {#each product.insulator ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-
-      </TableBodyCell>
-
-      <TableBodyCell>
-        {#if product.contacts}
-        {#each product.contacts ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-
-      </TableBodyCell>
-
-      <TableBodyCell>
-        {#if product.sealing}
-        {#each product.sealing ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-
-      </TableBodyCell>
-
-      <TableBodyCell>
-        {#if product.temperature}
-        {#each product.temperature ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-
-      </TableBodyCell>
-
-      <TableBodyCell>
-        {#if product.mating}
-        {#each product.mating ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-      </TableBodyCell>
-
-      <TableBodyCell>
-        {#if product.protection}
-        {#each product.protection ?? [] as entry}
-        <strong>{entry.title}:</strong> {entry.description}<br>
-        {/each}
-        {/if}
-      </TableBodyCell>
-    </TableBodyRow>
+  <!-- Iframe container -->
+  <div class="flex-1 ml-10">
+    <iframe title="{product.name}" src={product.pdf} width="100%" height="1100px" style="border:none;"></iframe>
+  </div>
 
 
-  </TableBody>
-</Table>
 </div>
