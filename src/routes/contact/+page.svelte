c33e0e5622a2ea204006da7b27dca9b0514d14b5
Teesitin <teesitin.russell@gmail.com>
2024-05-01 14:11:54 -0400

    dccontacts update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,13 +2,8 @@
     import GoogleMaps from "$lib/googleMaps/GoogleMaps.svelte";
 	import ContactsBanner from "$lib/pre-constructs/ContactsBanner.svelte";
     import Contacts from "$lib/pre-constructs/Contacts.svelte";
-
-    import { PhoneSolid, ClockSolid, MapPinAltSolid } from "flowbite-svelte-icons";
 </script>
 
 <GoogleMaps/>
-
-
 <ContactsBanner/>
-
 <Contacts/>
