3fedc5f1d71d08f9fa599d4dfb5ea1c571987249
Teesitin <teesitin.russell@gmail.com>
2024-05-01 12:59:23 -0400

    new entry for simple titles and updated prodcut cards

--- src/lib/homepage/products/ProductCard.svelte
+++ src/lib/homepage/products/ProductCard.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import { ChevronRightOutline } from 'flowbite-svelte-icons';
-    export let url:string;
+    import { type Product } from '$lib/data';
+
+    export let product:Product;
+
+    console.log(product);
 </script>
 
 <div class="lg:w-[400px] lg:h-[500px] flex flex-col justify-between rounded shadow-xl overflow-hidden bg-white">
@@ -11,40 +15,28 @@
     
     <!-- Title and description in the middle -->
     <div class="pl-7 pt-7 flex-1 flex flex-col">
-        
-            <span class="text-2xl font-semibold  text-primary-500 uppercase italic font-sans">
-                <slot name="title">
-                    CA Series Connectors
-                </slot>
-            </span>
-
-            <span class="text-[#C4C4C4] font-bold mt-4">COMPONENT MATERIALS</span>
-            
-        
-
-        <div class="flex text-md mt-1 text-[#6B7280]">
-            <span class="text-dark font-bold mr-1">Machined:</span> <slot name="desc1">
-                Lorem ipsum dolor sit 
+        <span class="text-2xl font-semibold  text-primary-500 uppercase italic font-sans">
+            <slot name="title">
+                CA Series Connectors
             </slot>
-            
-            
-        </div>
-        <div class="flex text-md mt-2 text-[#6B7280]">
-            <span class="text-dark font-bold mr-1">Die-cast: </span>
-            <slot name="desc2">
-                Lorem ipsum dolor sit 
-            </slot>     
-        </div>
-        <div class="flex text-md mt-2 text-[#6B7280]">
-            <span class="text-dark font-bold mr-1">Shell Plating:</span>
-            <slot name="desc3">
-                Lorem ipsum dolor sit 
-            </slot>     
-        </div>
-    </div> 
+        </span>
+
+        <span class="text-[#C4C4C4] font-bold mt-4">COMPONENT MATERIALS</span>
+        
+        {#if product.shell_mat}
+            {#each product.shell_mat.slice(0,3) as shellmat}
+                <div class="flex text-md mt-1 text-[#6B7280]">
+                    <span class="text-dark font-bold mr-1">{shellmat.simple_title ? shellmat.simple_title : shellmat.title}:</span>
+                    {shellmat.description}
+                </div>
+            {/each}
+        {/if}
+
+
+    </div>
 
     <!-- View Item at the bottom -->
-    <a class="px-7 pb-10 text-xl flex items-center justify-end text-primary-500 hover:text-primary-600 font-bold " href={url}>
+    <a class="px-7 pb-10 text-xl flex items-center justify-end text-primary-500 hover:text-primary-600 font-bold " href={product.url}>
         View <ChevronRightOutline class="ml-2" />
     </a>
 </div>
