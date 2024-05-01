d9c61176940ef16019f23cfdd8e8ef849e41af6e
Teesitin <teesitin.russell@gmail.com>
2024-05-01 13:59:37 -0400

    styling changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,44 +1,7 @@
 <script>
-	import ImageDesc from "$lib/pre-constructs/ImageDesc.svelte";
-import { CheckCircleOutline } from "flowbite-svelte-icons";
-
+	import AboutUs from "$lib/pre-constructs/AboutUs.svelte";
 </script>
 
 
-<div class="max-w-7xl m-auto my-40 flex gap-20">
-    <div class="w-[500px] bg-slate-300 aspect-square h-[500px] rounded">
-        <img src="/people/rep_1.webp" class="object-cover w-full h-full" alt="About Us">
-    </div>
+<AboutUs/>
 
-    <div class="w-1/2 flex flex-col gap-20">
-        <div class="text-left text-md">
-            <div class="text-3xl font-semibold text-m-primary mb-5">
-                About Us
-            </div>
-            <div class="text-justify text-sm">
-                At rde Connectors & Cables, Inc., we specialize in manufacturing industrial circular connectors for many types of electrical control and distribution systems. Applications include, but are not limited to, connectors for signal, sensor, power, and PCB interfaces, as well as controllers, encoders, and connector interfaces for robotics, feeder/delivery and assembly systems, including complete cable assemblies for any of the above systems. Custom configurations and prototyping are also possible. Our business facility is located in Sunrise, Florida, just west of Fort Lauderdale. Please click below for more information regarding our company:
-            </div>
-        </div>
-
-        <div class="text-left text-md ">
-            <div class="text-3xl font-semibold text-m-primary mb-5">
-                Mission Statement
-            </div>
-
-            <div class="flex text-justify text-sm mb-3">
-                <CheckCircleOutline class="mr-2 mt-[2px]"/>rde is committed to providing product quality and reliability of interconnect systems and assemblies for automation control in some of the most demanding and challenging industries and market applications.
-            </div>
-        
-            <div class="flex text-justify text-sm mb-3">
-                <CheckCircleOutline class="mr-2 mt-[2px]"/>We are dedicated to continuous improvement and enhancing our leadership position in our chosen markets.
-            </div>
-            
-            <div class="flex text-justify text-sm mb-3">
-                <CheckCircleOutline class="mr-2 mt-[2px]"/>We are committed to exceed our customer expectations in terms of quality, value and services.
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<!-- <ImageDesc/> -->
