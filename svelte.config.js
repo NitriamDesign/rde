7b334fd3b980bdf7b5da89f5e5804f781398e630
Noah <54770530+117GuiltySpark@users.noreply.github.com>
2024-10-03 18:34:32 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,28 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  preprocess: [vitePreprocess()],
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sitemapWrapAdapter } from 'sveltekit-static-sitemap';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // Wrap the static adapter with sitemap generation
+    adapter: sitemapWrapAdapter(
+      adapter({
+        // default options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: 'build',
+        assets: 'build',
+        fallback: undefined,
+        precompress: false,
+        strict: true
+      })
+    ),
+  },
+
+  preprocess: vitePreprocess(),
 };
+
+export default config;
